f862e7084a3bcbdd9618f9b61e85082841c3f50f
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-01-17 11:57:43 +0100

    creata classe astratta poligono

--- src/org/lessons/java/poligoni/AbstractPoligono.java
+++ src/org/lessons/java/poligoni/AbstractPoligono.java
@@ -2,6 +2,20 @@ package org.lessons.java.poligoni;
 
 public abstract class AbstractPoligono {
 
+    private String nome;
+
+    public AbstractPoligono(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public abstract int calcolaArea();
+
+    public abstract int calcolaPerimetro();
+
     
 
 }
