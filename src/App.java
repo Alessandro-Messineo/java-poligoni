1fbe00dd01eb672179baceac79a880678136fd6a
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-01-17 13:03:11 +0100

    testato codice

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import org.lessons.java.poligoni.Quadrato;
+import org.lessons.java.poligoni.Rettangolo;
+import org.lessons.java.poligoni.Triangolo;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Quadrato quadrato = new Quadrato("quadrato" , 20);
+        Rettangolo rettangolo = new Rettangolo("rettangolo", 18, 5);
+        Triangolo triangolo = new Triangolo("triangolo", 5, 17, 14);
+
+        System.out.println("---------------");
+        System.out.println(quadrato);
+        System.out.println("---------------");
+        System.out.println(rettangolo);
+        System.out.println("---------------");
+        System.out.println(triangolo);
+        System.out.println("---------------");
+
     }
 }
